21610411a577754de4bec747a35cee12137c0ae6
HypnoSpeaker <hypnospeaker@gmail.com>
2023-11-22 23:06:14 -0500

    post_survey complete, end_screen started

--- GodotProjects/Clicker Test/assets/scripts/post_survey.gd
+++ GodotProjects/Clicker Test/assets/scripts/post_survey.gd
@@ -1,6 +1,16 @@
 extends CanvasLayer
 
+var Q1 = false
+var Q2 = false
+var Q3 = false
+
+func _on_text_edit_text_changed():
+	Q1 = true
+	if Q1 == true && Q2 == true && Q3 == true:
+		$SubmitButton.disabled = false
+
 func _on_dog_slider_value_changed(value):
+	Q2 = true
 	if value == 0:
 		$DogCare.set_text("[center]I actively disregarded my dog.")
 	elif value == 1:
@@ -13,8 +23,16 @@ func _on_dog_slider_value_changed(value):
 		$DogCare.set_text("[center]I kept my dog's stats in peak condition.")
 	value += 1
 	Global.DogCare = value
+	if Q1 == true && Q2 == true && Q3 == true:
+		$SubmitButton.disabled = false
 
 func _on_game_slider_value_changed(value):
+	Q3 = true
 	value += 1
 	$GameRating.set_text("[center]" + str(value))
 	Global.GameRating = value
+	if Q1 == true && Q2 == true && Q3 == true:
+		$SubmitButton.disabled = false
+
+
+	
